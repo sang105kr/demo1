d406a4a7e28355e45753abbdc9e6bea6e8ac940b
sanghakyi <sang105kr@gmail.com>
2023-10-10 09:52:11 +0900

    '20231010-01'

--- src/main/java/com/kh/demo1/web/MemberController.java
+++ src/main/java/com/kh/demo1/web/MemberController.java
@@ -1,5 +1,6 @@
 package com.kh.demo1.web;
 
+import com.kh.demo1.domain.dao.entity.Member;
 import com.kh.demo1.domain.svc.MemberSVC;
 import com.kh.demo1.web.form.member.JoinForm;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,21 @@ public class MemberController {
   //가입처리
   @PostMapping("/add")
   public String join(JoinForm joinForm){
+
     log.info("join()호출");
     log.info("joinForm={}",joinForm);
+
+    Member member = new Member();
+    member.setEmail(joinForm.getEmail());
+    member.setPasswd(joinForm.getPasswd());
+    member.setTel(joinForm.getTel());
+    member.setNickname(joinForm.getNickname());
+    member.setGender(joinForm.getGender());
+    member.setHobby(joinForm.getHobby());
+    member.setRegion(joinForm.getRegion());
+    member.setGubun("M0101"); //일반회원
+    Member joinedMember = memberSVC.join(member);
+
     return "index"; // view 이름
   }
 
