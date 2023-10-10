82c59a8510030642ee4c92d5ca3c735e65ce73a5
sanghakyi <sang105kr@gmail.com>
2023-10-10 16:14:46 +0900

    '20231010-7-1'

--- src/main/java/com/kh/demo1/web/MemberController.java
+++ src/main/java/com/kh/demo1/web/MemberController.java
@@ -3,12 +3,19 @@ package com.kh.demo1.web;
 import com.kh.demo1.domain.dao.entity.Member;
 import com.kh.demo1.domain.svc.MemberSVC;
 import com.kh.demo1.web.form.member.JoinForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,20 +25,41 @@ public class MemberController {
 
   private final MemberSVC memberSVC;
 
+  //취미
+  @ModelAttribute("hobbys")   // model.addAttributes("hobbys",hobbys);
+  public Map<String,String> hobbys(){
+    Map<String,String> hobbys = new HashMap<>();
+    hobbys.put("swim","수영");
+    hobbys.put("climing", "등산");
+    hobbys.put("golf", "골프");
+    return hobbys;
+  }
+
   //가입화면
   @GetMapping("/add")      //  GET http://localhost:9080/members/add
-  public String joinForm(){
+  public String joinForm(Model model){
 
+    model.addAttribute("joinForm",new JoinForm());
     return "member/joinForm";
   }
 
 
   //가입처리
   @PostMapping("/add")      //  POST http://localhost:9080/members/add
-  public String join(JoinForm joinForm){
+  public String join(
+      @Valid                          //form객체의 필드 유효성 체크, 생략하면 유효성 체크 안함.
+      @ModelAttribute                 //Model객체에 form객체타입을 키로 form객체 값을 벨류로 저장함.
+      JoinForm joinForm,
+      BindingResult bindingResult     // 오류필드와 오류메시지를 담고있는 객체로 form객체 바로다음에 와야함.
+  ){
 
     log.info("join()호출");
     log.info("joinForm={}",joinForm);
+
+    if(bindingResult.hasErrors()){
+      log.info("bindingResult={}", bindingResult);
+      return "member/joinForm";
+    }
 
     Member member = new Member();
     member.setEmail(joinForm.getEmail());
@@ -39,7 +67,7 @@ public class MemberController {
     member.setTel(joinForm.getTel());
     member.setNickname(joinForm.getNickname());
     member.setGender(joinForm.getGender());
-    member.setHobby(joinForm.getHobby());
+//    member.setHobby(joinForm.getHobby());
     member.setRegion(joinForm.getRegion());
     member.setGubun("M0101"); //일반회원
     Member joinedMember = memberSVC.join(member);
--- src/main/java/com/kh/demo1/web/form/member/JoinForm.java
+++ src/main/java/com/kh/demo1/web/form/member/JoinForm.java
@@ -1,16 +1,27 @@
 package com.kh.demo1.web.form.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.List;
 
 //@Getter@Setter@ToString
 @Data
 public class JoinForm {
+  @Email
+  @Max(50)
   private String email;
+
+  @Size(min=4,max=12)
+  @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$")
   private String passwd;
   private String tel;
   private String nickname;
   private String gender;
-  private String hobby;
+  private List<String> hobby;
   private String region;
   private byte[] pic;
 }
