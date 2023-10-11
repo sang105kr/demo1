e501476eab5eb7810451bd15e03d5640a98d9b20
sanghakyi <sang105kr@gmail.com>
2023-10-11 17:32:05 +0900

    '20231011-8'

--- src/main/java/com/kh/demo1/web/LoginController.java
+++ src/main/java/com/kh/demo1/web/LoginController.java
@@ -1,7 +1,10 @@
 package com.kh.demo1.web;
 
+import com.kh.demo1.domain.dao.entity.Member;
+import com.kh.demo1.domain.svc.MemberSVC;
 import com.kh.demo1.web.form.login.LoginForm;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+
+  private final MemberSVC memberSVC;
 
   //로그인 화면
   @GetMapping("login")
@@ -34,6 +42,22 @@ public class LoginController {
       return "login/loginForm";
     }
 
+    Optional<Member> optionalMember = memberSVC.findByEmail(loginForm.getEmail());
+    //1) 아이디가 없는경우
+    if(optionalMember.isEmpty()){
+      bindingResult.reject("invalidMember",null);
+    }else{
+      Member member = optionalMember.get();
+      //2)비밀번호가 다른경우
+      if(!member.getPasswd().equals(loginForm.getPasswd())){
+        bindingResult.reject("invalidMember",null);
+      }
+    }
+
+    if(bindingResult.hasErrors()){
+      log.info("bindingResult={}", bindingResult);
+      return "login/loginForm";
+    }
 
     return "index";
   }
