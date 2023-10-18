944531f33e4a256fe3843219ac41a583afbbf762
sanghakyi <sang105kr@gmail.com>
2023-10-18 18:13:48 +0900

    '20231018-8-4'

--- src/main/java/com/kh/demo1/web/api/ApiResponse.java
+++ src/main/java/com/kh/demo1/web/api/ApiResponse.java
@@ -1,7 +1,7 @@
 package com.kh.demo1.web.api;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ClassUtils;
 
@@ -9,18 +9,29 @@ import java.util.Collection;
 import java.util.Map;
 
 @Slf4j
-@Data
-@AllArgsConstructor
+@Getter
+@ToString
 public class ApiResponse<T> {
   private Header header;    //응답헤더
   private T body;           //응답바디
   private int totalCnt;     //총건수
 
-  @Data
-  @AllArgsConstructor
-  public static class Header{
+  private ApiResponse(Header header, T body, int totalCnt) {
+    this.header = header;
+    this.body = body;
+    this.totalCnt = totalCnt;
+  }
+
+  @Getter
+  @ToString
+  private static class Header{
     private String rtcd;      //응답코드
     private String rtmsg;     //응답메시지
+
+    Header(String rtcd, String rtmsg) {
+      this.rtcd = rtcd;
+      this.rtmsg = rtmsg;
+    }
   }
 
   public static <T> ApiResponse<T> createApiResponse(String rtcd, String rtmsg, T body){
